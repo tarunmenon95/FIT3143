897e89caf779520f7f2e55fc89727fb9310a87a3
Tarun <tarunmenon95@gmail.com>
2020-08-19 10:27:26 +1000

    Commenting and fixed time on serial.c

--- lab_week3/serial.c
+++ lab_week3/serial.c
@@ -5,16 +5,21 @@
 #include <time.h>
 
 int main(int argc, char* argv[]) {
-    int write_to_file(long* arr, size_t len);
-    struct timespec start, end;
-    double totalTime;
-
-
+   
     //Get user input for n
     int n;
     scanf ("%d",&n);
+
+    struct timespec startProgram, endProgram, startComp, endComp;
     
-    //Check whether num is prime
+    //Get program start time
+    clock_gettime(CLOCK_MONOTONIC, &startProgram); 
+    
+    int write_to_file(long* arr, size_t len);
+    double totalTime;
+     
+     
+     //Check whether num is prime
     int is_prime(long i) {
     long l = (long)ceil(sqrt((double)i));
     for (long j = 2; j <= l; ++j) {
@@ -24,10 +29,12 @@ int main(int argc, char* argv[]) {
     }
     return 1;  // is prime
     }
-    
-    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    //Get computation start time
+    clock_gettime(CLOCK_MONOTONIC, &startComp);
     int primes[n];
 
+    //Find all primes and add to array
     int counter = 0;
     for (int i = 1; i <= n; i++){
         if (is_prime(i))
@@ -36,7 +43,18 @@ int main(int argc, char* argv[]) {
             counter++;
         }
     }
+    
+    //Get computation end time
+    clock_gettime(CLOCK_MONOTONIC, &endComp);
 
+    //Output computation time
+    totalTime = (endComp.tv_sec - startComp.tv_sec) * 1e9; 
+    totalTime= (totalTime + (endComp.tv_nsec - startComp.tv_nsec)) * 1e-9; 
+    printf("%lf", totalTime);
+    printf("%s", "\n");
+
+
+    //Open output primes.txt file
     FILE* f = fopen("primes.txt", "w");
 
     for (int i = 0; i < counter; i++)
@@ -47,11 +65,16 @@ int main(int argc, char* argv[]) {
 
     fclose(f);
 
-    clock_gettime(CLOCK_MONOTONIC, &end);
 
-   totalTime = (end.tv_sec - start.tv_sec) * 1e9; 
-   totalTime= (totalTime + (end.tv_nsec - start.tv_nsec)) * 1e-9; 
+    //Get program end time and output
+    clock_gettime(CLOCK_MONOTONIC, &endProgram);
+    totalTime = (endProgram.tv_sec - startProgram.tv_sec) * 1e9; 
+    totalTime= (totalTime + (endProgram.tv_nsec - startProgram.tv_nsec)) * 1e-9; 
 
-   printf("%lf", totalTime);
+
+    printf("%lf", totalTime);
+
+
+   
 
 }
