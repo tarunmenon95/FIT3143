900b03dc7e4caea024d0beb18c26c4306a00e0b6
Jeffrey Vo <jvo9@student.monash.edu>
2020-08-19 10:24:25 +1000

    Updated parallel primes

--- lab_week3/parallel.c
+++ lab_week3/parallel.c
@@ -18,13 +18,19 @@ void* find_primes_thread(void* arg);
 size_t find_primes(long start, long end, long increment, long* ptr);
 
 int main(int argc, char* argv[]) {
+    struct timespec start, finish;
+    double elapsed, total_time = 0;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    double NANOSECONDS_IN_SECOND = 1000000000.0;
+
+    // n = search for primes < n
     // t = no. of threads
     if (argc != 3) {
         printf("Usage: %s n [t]\n", argv[0]);
         return 1;
     }
 
-    // get N
+    // get n
     char* ptr;
     long n = strtol(argv[1], &ptr, 10);
     if (ptr == argv[1]) {
@@ -32,10 +38,9 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // if parallel get number of threads
-    long number_threads;
+    // get number of threads
     ptr = NULL;
-    number_threads = strtol(argv[2], &ptr, 10);
+    long number_threads = strtol(argv[2], &ptr, 10);
     if (ptr == argv[2]) {
         printf("Couldn't convert t\n");
         return 1;
@@ -46,6 +51,9 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
+    // array to store primes
+    // upper limit to no. of primes < n is n/2
+    // there's probably a tighter upper bound but this is good enough
     long* primes = (long*)malloc((size_t)(n / 2) * sizeof(*primes));
     if (!primes) {
         printf("Could not malloc\n");
@@ -55,17 +63,27 @@ int main(int argc, char* argv[]) {
     primes[0] = 2;
     size_t primes_count = 1;
 
-    struct timespec start, finish;
-    double elapsed;
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    elapsed = (finish.tv_sec - start.tv_sec) +
+              (finish.tv_nsec - start.tv_nsec) / NANOSECONDS_IN_SECOND;
+    total_time += elapsed;
+    printf("Initial setup: %.3f s\n", elapsed);
     clock_gettime(CLOCK_MONOTONIC, &start);
+
+    // info that each thread needs for their computations
     pthread_t tid[number_threads];
     PrimeCalcInfo* infos =
         (PrimeCalcInfo*)malloc((size_t)number_threads * sizeof(*infos));
     printf("Spawning %ld threads\n", number_threads);
     for (int i = 0; i < number_threads; ++i) {
+        // multiply by 2 to ignore even numbers
+        // each thread will start with a different number
         infos[i].start = 3 + 2 * i;
         infos[i].end = n;
+        // each thread jumps forward the same number, hence will cover all
+        // numbers
         infos[i].increment = 2 * number_threads;
+        // each thread will have their own array to store the found primes
         infos[i].ptr =
             (long*)malloc((size_t)(n / 2 / number_threads) * sizeof(long));
         pthread_create(&tid[i], NULL, find_primes_thread, &infos[i]);
@@ -75,21 +93,22 @@ int main(int argc, char* argv[]) {
         pthread_join(tid[i], NULL);
     }
     printf("Copying\n");
+    // from each thread's array of primes, copy into the main array
+    // simple copy, WILL NOT BE SORTED
     for (int i = 0; i < number_threads; ++i) {
         for (size_t k = 0; k < infos[i].count; ++k) {
-            primes[primes_count + k] = infos[i].ptr[k];
+            primes[primes_count++] = infos[i].ptr[k];
         }
         free(infos[i].ptr);
-        primes_count += infos[i].count;
     }
     free(infos);
 
     clock_gettime(CLOCK_MONOTONIC, &finish);
-    elapsed = (finish.tv_sec - start.tv_sec);
-    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-
-    printf("Found %ld primes less than %ld\nTime taken: %.2f s\n", primes_count,
-           n, elapsed);
+    elapsed = (finish.tv_sec - start.tv_sec) +
+              (finish.tv_nsec - start.tv_nsec) / NANOSECONDS_IN_SECOND;
+    total_time += elapsed;
+    printf("Finding primes: %.3f s\n", elapsed);
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
     if (write_to_file(primes, primes_count)) {
         return 1;
@@ -98,6 +117,16 @@ int main(int argc, char* argv[]) {
     printf("Written\n");
 
     free(primes);
+
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    elapsed = (finish.tv_sec - start.tv_sec) +
+              (finish.tv_nsec - start.tv_nsec) / NANOSECONDS_IN_SECOND;
+    total_time += elapsed;
+    printf("Writing to file: %.3f s\n", elapsed);
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    printf("Found %ld primes less than %ld\n", primes_count, n);
+    printf("Time taken: %.3f s\n", total_time);
 
     return 0;
 }
@@ -122,10 +151,10 @@ int is_prime(long i) {
     long l = (long)ceil(sqrt((double)i));
     for (long j = 2; j <= l; ++j) {
         if (i % j == 0) {
-            return 0;  // not prime
+            return 0;  // found a divisor, not prime
         }
     }
-    return 1;  // is prime
+    return 1;  // no divisors, is prime
 }
 
 int write_to_file(long* arr, size_t len) {
